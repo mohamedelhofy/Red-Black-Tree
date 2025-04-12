3f66aa5b1fa31020a1fda956e5029421b2e8e25a
mohamedelhofy <alhofymohamed@gmail.com>
2025-04-12 10:58:27 +0200

    Add class Node

---
+++ red_black_tree.py
@@ -0,0 +1,8 @@
+class Node :
+    def __init__(self, key=None, color='Red'):
+        self.key=key
+        self.color=color
+        self.left = None
+        self.right = None
+        self.parent = None
+        pass
